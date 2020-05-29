fe4d906a779960130e16cdd8490d21c32668afb3
NAMAN <namansingh0305@gmail.com>
2020-05-29 23:27:41 +0530

    a2oj

--- codeforce/ecr_81_C.cpp
+++ codeforce/ecr_81_C.cpp
@@ -8,69 +8,58 @@ int solve(){
 	string stra,strb;
 	cin>>stra>>strb;
 
-	vector< int > counta(26,0);
-	vector< int > countb(26,0);
+	vector< int > count(26,0);
 	queue < char > character;
 
 	int lngthb = strb.length();
 
 	for( int i = 0; i < stra.length(); i++ ){		
-		int x = (int)stra[i] - 97;
-		counta[x]++;
+		count[ stra[i] - 'a' ]++;
 	}
 
 	for( int i = 0; i < strb.length(); i++ ){
-		int x = (int)strb[i] - 97;
-		countb[x]++;
-	}
-	
-	for( int i = 0; i < 26; i++ ){
-		if( countb[i] > 0 && counta[i] == 0 ){
+		if(count[ strb[i] - 'a' ] == 0){
 			cout<<"-1"<<endl;
-			return 1;	
+			return 1;
 		}
 	}
 
 	int x;
 	
 	for( int i = 0; i < stra.length(); i++ ){
-		x = (int)stra[i] - 97;
-		
-		if( countb[x] ){
+		if( count[stra[i] - 'a'] ){
 			character.push( stra[i] );
+		
 		}
-
 	}
 
 	character.push( '#' );
 
 	char m;
-	int count=1,j=0;
+	int cnt=1,j=0;
 	int w=10;
 
-	while( lngthb ){
+	while( w-- ){
 
 		m = character.front();
 		character.pop();
 
 		if ( m == '#') {
 			character.push( '#' );
-			count++;continue;
+			cnt++;continue;
 		}
 
 		if( strb[j] == m ){
-			
-			countb[ (int)strb[ j ] - 97 ]--;
+			count[ strb[ j ] - 'a' ]--;
 			j++;lngthb--;
-			
 		}
 
-		if( countb[ (int)m - 97 ] ){
+		if( count[ m - 'a' ] ){
 			character.push( m );
 		}
 
 	}
-	cout<<count<<endl;
+	cout<<cnt<<endl;
 	return 1;
 }
 
